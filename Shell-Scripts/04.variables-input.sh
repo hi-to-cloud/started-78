1859b16aaf6ebad25a5dcde58317c7a0a303c34e
Naveen <hi.to.cloud@gmail.com>
2024-10-19 22:05:32 +0530

    Added Shell

--- Shell-Scripts/04.variables-input.sh
+++ Shell-Scripts/04.variables-input.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Please enter your username:: "
 # if give `read USERNAME` it will print the username in CLI
-# for `read -s USERNAME` it wont print the username in CLI, -s=secure
-read -s USERNAME #USERNAME ..> variable
+# for `read -s PASSWORD` it wont print the password in CLI, -s=secure
+read USERNAME #USERNAME ..> variable
 echo "Please enter your Password:: "
 read -s PASSWORD #PASSWORD ..> variable
 echo "Username is: $USERNAME, Password is: $PASSWORD"
