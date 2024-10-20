e87cd1cbc54d6b413924d1356d000624eef01c89
Naveen <hi.to.cloud@gmail.com>
2024-10-20 15:59:58 +0530

    Added Shell

---
+++ Shell-Scripts/11.share-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Share Variables Between 2 Shell Scripts
+# chmod +x xx.sh
+# 2 ways ..> ./xx.sh 
+      #  ..> source ./xx.sh
+# https://medium.com/@kalidindi.naveen.2000/share-variables-between-2-shell-scripts-a33791429891
